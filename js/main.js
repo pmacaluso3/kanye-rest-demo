abf4a7b533c4fea527909709605d918cba25c850
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-02-01 16:28:03 -0500

    demo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("👀")
+async function kanye() {
+  const res = await fetch('https://api.kanye.rest/')
+  const json = await res.json()
+
+  document.querySelector('#display-quote').innerText = json.quote
+}
+
+document.querySelector('#get-quote').addEventListener('click', kanye)
+
+kanye()
